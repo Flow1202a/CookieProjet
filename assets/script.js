c567f1e83a67fe3bba653f014376c549b3c1b750
Flow1202a <aliuflorian8@gmail.com>
2024-06-19 16:44:27 +0200

    save

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+let counterDisplayElem = document.querySelector('.counter-display');
+let counterPlusElem = document.querySelector('.counter-plus');
+
+let count = 0;
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+};
+
