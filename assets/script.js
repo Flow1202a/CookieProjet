ba9d44e9ccab4364e605512a5b6e644aed16c8fa
Flow1202a <aliuflorian8@gmail.com>
2024-06-21 14:14:41 +0200

    update impact click js html and css

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,9 @@ let count = 0;
 let countCpm = 5//ajouter les multiplicateur ici
 
 
-
+//update
 updateCounter();
 updateCounterCpm();
-
-
 
 
 //clicker
@@ -46,8 +44,32 @@ document.getElementById('toggleButton').addEventListener('click', function() {
     }
 });
 
-//Fonctions
+//impact click
+let btn = document.querySelector(".counter-plus");
+let animationInProgress = false;
+let animationId;
+btn.addEventListener("click", (e) => {
+    const clickEffect = document.querySelector(".click-effect");
 
+    if (animationInProgress) {
+        clearTimeout(animationId);
+        clickEffect.classList.remove("effect");
+        void clickEffect.offsetWidth;
+    }
+
+    clickEffect.style.top = e.clientY + window.scrollY + "px";
+    clickEffect.style.left = e.clientX + window.scrollX + "px";
+    clickEffect.classList.add("effect");
+    animationInProgress = true;
+
+    animationId = setTimeout(() => {
+        clickEffect.classList.remove("effect");
+        animationInProgress = false;
+    }, 750);
+});
+
+
+//Fonctions
 
 //update du compteur global
 function updateCounter(){
@@ -57,6 +79,8 @@ function updateCounter(){
 function updateCounterCpm(){
     cpm.innerHTML = countCpm;
 }
+
+
 
 
 
